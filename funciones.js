0552822be4d0e39fca971039f2999e61a9174e48
Daniel Piantelli <piantellidaniel@gmail.com>
2023-03-28 13:54:31 -0300

    segunda entrega del proyecto

---
+++ funciones.js
@@ -0,0 +1,15 @@
+
+document.getElementById("inicio").onclick= function (){
+   window.location.href = "index.html";
+}
+
+document.getElementById("experiencia").onclick= function (){
+    window.location.href = "experiencia.html";
+ }
+ 
+document.getElementById("contacto").onclick= function (){
+    window.location.href = "contacto.html";
+ }
+
+
+ 
